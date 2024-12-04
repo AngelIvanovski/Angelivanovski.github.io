5d40c11bc69d9570e43b7a614b03329562841b1c
AngelIvanovski <angel.ivanovski98@gmail.com>
2024-12-04 19:44:22 +0100

    variables homework

---
+++ javascript/homework-1/script.js
@@ -0,0 +1,9 @@
+
+let priceOfPhone = 119.95;
+let numPhones = 30;
+let tax = 5 / 100;
+let priceWithTax = priceOfPhone + priceOfPhone * tax;
+let total = numPhones * priceWithTax;
+
+console.log("This is the price of 30 phones with tax:");
+console.log(total);
